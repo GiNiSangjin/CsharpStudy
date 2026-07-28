c234965b946d5394d728eb942a97d4d6797f6422
GiNiSangjin <jinpang97@ajou.ac.kr>
2026-07-28 16:35:28 +0900

    초기 커밋.

---
+++ WriteValueParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CsharpStudy
+{
+    internal enum WriteValueParseResult
+    { 
+        Success,
+        NotNumber,
+        OutOfRange
+    }
+    internal static class WriteValueParser
+    {
+        public static WriteValueParseResult TryParseWriteValue(string input, out int value)
+        {
+            value = 0;
+            if(!int.TryParse(input, out int parsedValue))
+            {
+                return WriteValueParseResult.NotNumber;
+            }
+            if(parsedValue < 0 || parsedValue > 32767)
+            {
+                return WriteValueParseResult.OutOfRange;
+            }
+            value = parsedValue;
+            return WriteValueParseResult.Success;
+        }
+
+    }
+}
