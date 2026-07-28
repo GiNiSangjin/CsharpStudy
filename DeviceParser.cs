e0b4c15305dd783f1d0146427c76d551ff0df073
GiNiSangjin <jinpang97@ajou.ac.kr>
2026-07-29 08:57:21 +0900

    Device Parsing Logic Separates DeviceParser Class

--- DeviceParser.cs
+++ DeviceParser.cs
@@ -8,5 +8,87 @@ namespace CsharpStudy
 {
     internal class DeviceParser
     {
+        private static readonly List<string> ValidDeviceTypes = new List<string> { "D", "M", "ZR" };
+
+        internal static DeviceParseInfo ParseDevice(string device)
+        {
+            if (string.IsNullOrWhiteSpace(device))
+            {
+                return new DeviceParseInfo(DeviceParseResult.EmptyInput, "", 0);
+            }
+
+            device = device.Trim();
+
+            int firstDigitIndex = FindFirstDigitIndex(device);
+
+            if (firstDigitIndex == -1)
+            {
+                return new DeviceParseInfo(DeviceParseResult.InvalidAddress, "", 0);
+            }
+
+            if (firstDigitIndex == 0)
+            {
+                return new DeviceParseInfo(DeviceParseResult.MissingDeviceType, "", 0);
+            }
+
+            if (!IsAllLetters(device, firstDigitIndex))
+            {
+                return new DeviceParseInfo(DeviceParseResult.InvalidDeviceTypeFormat, "", 0);
+            }
+            string parsedDeviceType = device.Substring(0, firstDigitIndex).ToUpper();
+
+            if (!IsSupportedDeviceType(parsedDeviceType))
+            {
+                return new DeviceParseInfo(DeviceParseResult.UnsupportedDeviceType, "", 0);
+            }
+
+            string addressText = device.Substring(firstDigitIndex);
+
+            if (TryParseAddress(addressText, out int parsedAddress))
+            {
+                return new DeviceParseInfo(DeviceParseResult.Success, parsedDeviceType, parsedAddress);
+            }
+
+            return new DeviceParseInfo(DeviceParseResult.InvalidAddress, "", 0);
+        }
+        static int FindFirstDigitIndex(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static bool IsAllLetters(string text, int endIndex)
+        {
+            for (int i = 0; i < endIndex; i++)
+            {
+                if (!char.IsLetter(text[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool IsSupportedDeviceType(string deviceType)
+        {
+            return ValidDeviceTypes.Contains(deviceType);
+        }
+
+        static bool TryParseAddress(string addressText, out int address)
+        {
+            if (int.TryParse(addressText, out int parsedAddress)
+                && parsedAddress >= 0)
+            {
+                address = parsedAddress;
+                return true;
+            }
+
+            address = 0;
+            return false;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace CsharpStudy
     
     internal class Program
     {
-        private static readonly List<string> ValidDeviceTypes = new List<string> { "D", "M", "ZR" };
+        
         static void Main(string[] args)
         {
             Console.Write("Device 입력: ");
             string input = (Console.ReadLine() ?? "").Trim();
 
-            DeviceParseInfo info = ParseDevice(input);
+            DeviceParseInfo info = DeviceParser.ParseDevice(input);
 
             string message = GetDeviceResultMessage(
                 info.Result, info.DeviceType, info.Address);
@@ -20,89 +20,6 @@ namespace CsharpStudy
             Console.WriteLine(message);
             
 
-        }
-
-        static int FindFirstDigitIndex(string text)
-        {
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (char.IsDigit(text[i]))
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-        static bool IsAllLetters(string text, int endIndex)
-        {
-            for (int i = 0; i < endIndex; i++)
-            {
-                if (!char.IsLetter(text[i]))
-                    return false;
-            }
-
-            return true;
-        }
-
-        static bool IsSupportedDeviceType(string deviceType)
-        {
-            return ValidDeviceTypes.Contains(deviceType);
-        }
-
-        static bool TryParseAddress(string addressText, out int address)
-        {
-            if (int.TryParse(addressText, out int parsedAddress)
-                && parsedAddress >= 0)
-            {
-                address = parsedAddress;
-                return true;
-            }
-
-            address = 0;
-            return false;
-        }
-
-        static DeviceParseInfo ParseDevice(string device)
-        {
-            if(string.IsNullOrWhiteSpace(device))
-            {
-                return new DeviceParseInfo(DeviceParseResult.EmptyInput, "", 0);
-            }
-            
-            device = device.Trim();
-
-            int firstDigitIndex= FindFirstDigitIndex(device);
-
-            if(firstDigitIndex == -1) 
-            {
-                return new DeviceParseInfo(DeviceParseResult.InvalidAddress, "", 0);
-            }
-
-            if(firstDigitIndex == 0)
-            {
-                return new DeviceParseInfo(DeviceParseResult.MissingDeviceType, "", 0);
-            }
-
-            if(!IsAllLetters(device, firstDigitIndex))
-            {
-                return new DeviceParseInfo(DeviceParseResult.InvalidDeviceTypeFormat, "", 0);
-            }
-            string parsedDeviceType = device.Substring(0, firstDigitIndex).ToUpper();
-
-            if (!IsSupportedDeviceType(parsedDeviceType))
-            {
-                return new DeviceParseInfo(DeviceParseResult.UnsupportedDeviceType, "", 0);
-            }
-
-            string addressText = device.Substring(firstDigitIndex);
-
-            if(TryParseAddress(addressText, out int parsedAddress))
-            {
-                return new DeviceParseInfo(DeviceParseResult.Success, parsedDeviceType, parsedAddress);
-            }
-
-            return new DeviceParseInfo(DeviceParseResult.InvalidAddress, "", 0);
         }
 
         static string GetDeviceResultMessage(DeviceParseResult result, string deviceType, int address)
@@ -135,56 +52,6 @@ namespace CsharpStudy
                     
             }
         }
-        
-        static DeviceParseResult TryParseDevice(string device, out string deviceType, out int address)
-        {
-            deviceType = "";
-            address = 0;
-
-            if (string.IsNullOrWhiteSpace(device))
-            {
-                return DeviceParseResult.EmptyInput;
-            }
-
-            device = device.Trim();
-
-            int firstDigitIndex = FindFirstDigitIndex(device);
-
-            if (firstDigitIndex == -1)
-            {
-                return DeviceParseResult.InvalidAddress;
-            }
-
-            if (firstDigitIndex == 0)
-            {
-                return DeviceParseResult.MissingDeviceType;
-            }
-
-            if(!IsAllLetters(device, firstDigitIndex))
-            {
-                return DeviceParseResult.InvalidDeviceTypeFormat;
-
-            }
-
-            string parsedDeviceType = device.Substring(0, firstDigitIndex).ToUpper();
-
-            if(!IsSupportedDeviceType(parsedDeviceType))
-            {
-                return DeviceParseResult.UnsupportedDeviceType;
-            }
-
-            string addressText = device.Substring(firstDigitIndex);
-
-            if (TryParseAddress(addressText, out address)) 
-            {
-                deviceType = parsedDeviceType;
-                return DeviceParseResult.Success;
-            }
-
-
-            return DeviceParseResult.InvalidAddress;
-        }
-
         
     }
 }
